d596863bfbcf56b616b21cb522b3fa90d1555274
FAIZ3000 <59557773+FAIZ3000@users.noreply.github.com>
2020-07-17 20:29:43 +0800

    2020.07.17

---
+++ backend/routes/uploadRoute.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import aws from 'aws-sdk';
+import config from '../config';
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'frontend/public/images');
+  },
+  filename(req, file, cb) {
+    cb(null, `${Date.now()}.jpg`);
+  },
+});
+
+const upload = multer({ storage });
+
+const router = express.Router();
+
+router.post('/', upload.single('image'), (req, res) => {
+  //res.send(`/${req.file.path}`);
+  res.send("/images/" + req.file.filename)
+});
+
+/*aws.config.update({
+  accessKeyId: config.accessKeyId,
+  secretAccessKey: config.secretAccessKey,
+});
+const s3 = new aws.S3();
+const storage = multerS3({
+  s3,
+  bucket: 'toyshop-bucket',
+  acl: 'public-read',
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+  key(req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const uploadS3 = multer({ storage: storage });
+router.post('/s3', uploadS3.single('image'), (req, res) => {
+  res.send(req.file.location);
+});*/
+export default router;
